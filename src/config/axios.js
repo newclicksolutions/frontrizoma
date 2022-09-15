7d7e8c7c16ca209f0511284e6e7052b80dda86f3
Jonathan Madrid <jonathanm@propelapps.com>
2022-09-15 14:18:49 -0500

    localhost

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const clientAxios = axios.create({
   // baseURL: "https://api-apprizoma.herokuapp.com",
-  baseURL: "http://localhost:8000/",
+  baseURL: "http://localhost:8000",
   headers: {
     "Content-Type": "application/json",
     Accept: "*/*",
