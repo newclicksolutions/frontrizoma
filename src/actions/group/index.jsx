d51ee1ef936e6a28e47b5775d9a2945fb8c57aad
Jonathan Madrid <jonathanm@propelapps.com>
2023-02-10 15:15:47 -0500

    drag and drop

--- src/actions/group/index.jsx
+++ src/actions/group/index.jsx
@@ -35,7 +35,7 @@ export const exportExcelApi = () => {
                 });
             } catch (error) {}
           }else {
-            dispatch(obtainGroupByCommunity(null));
+            dispatch(obtainGroupByCommunity(result.data));
           }
           // if (
           //   result.data.data.message == "Import succesfully" ||
